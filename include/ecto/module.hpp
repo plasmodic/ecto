8736bb5cd48d8a4acee6c1653d9eadb714df3ced
Troy Straszheim <straszheim@willowgarage.com>
2011-03-18 09:01:44 -0700

    that wasn't really an impl

--- include/ecto/module.hpp
+++ include/ecto/module.hpp
@@ -23,28 +23,52 @@ namespace ecto
     bool dirty() const;
 
     template<typename T>
-    tendril& 
-    setOut(const std::string& name, const std::string& doc = "", const T& t = T());
+    tendril& setOut(const std::string& name,
+		    const std::string& doc = "",
+		    const T& t = T())
+    {
+      outputs[name] = ecto::tendril::make<T>(t,doc);
+      return outputs[name];
+    }
 
     template<typename T>
-    tendril& 
-    setIn(const std::string& name, const std::string& doc = "", const T& t = T());
+    tendril& setIn(const std::string& name,
+		   const std::string& doc = "",
+		   const T& t = T())
+    {
+      inputs[name] = ecto::tendril::make<T>(t,doc);
+      return inputs[name];
+    }
 
     template<typename T>
-    tendril& 
-    setParam(const std::string& name, const std::string& doc = "", const T& t = T());
+    tendril& setParam(const std::string& name,
+		      const std::string& doc = "",
+		      const T& t = T())
+    {
+      params[name] = ecto::tendril::make<T>(t,doc);
+      return params[name];
+    }
 
-    template <typename T>
-    T& 
-    getOut(const std::string& name);
-    
-    template <typename T>
-    const T& 
-    getIn(const std::string& name);
+    template<typename T>
+    T& getOut(const std::string& name)
+    {
+      //FIXME check for null, throw
+      return outputs[name].get<T>();
+    }
 
-    template <typename T>
-    const T& 
-    getParam(const std::string& name);
+    template<typename T>
+    const T& getIn(const std::string& name)
+    {
+      //FIXME check for null, throw
+      return inputs[name].get<T>();
+    }
+
+    template<typename T>
+    const T& getParam(const std::string& name)
+    {
+      //FIXME check for null, throw
+      return params[name].get<T>();
+    }
 
 
     tendrils_t inputs, outputs, params;
@@ -55,7 +79,4 @@ namespace ecto
     friend class plasm;
   };
 
-
-
-#include <ecto/impl/module.hpp>
 }//namespace ecto
