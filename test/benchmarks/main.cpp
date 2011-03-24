9b994096aed34786a3bc1f967cce53d14e9d3711
Ethan Rublee <ethan.rublee@gmail.com>
2011-03-23 23:35:09 -0700

    Adding benchmarks...

---
+++ test/benchmarks/main.cpp
@@ -0,0 +1,61 @@
+#include "push_ups.cpp"
+
+#include <iomanip>
+#include <boost/progress.hpp>
+#include <boost/timer.hpp>
+
+int main()
+{
+  ecto::plasm p;
+  ecto::module::ptr m(new ecto_push_ups::Add2());
+  m->Config();
+
+  ecto::module::ptr b(new ecto_push_ups::BigData());
+  b->Config();
+
+  std::cout << "adding (" << std::endl;
+  std::cout << "ecto" << std::endl;
+  {
+    boost::progress_timer t; // start timing
+    for (size_t i = 0; i < 10e6; i++)
+    {
+      p.markDirty(m);
+      p.go(m);
+    }
+  }
+  std::cout << "raw" << std::endl;
+  {
+    boost::progress_timer t; // start timing
+    for (size_t i = 0; i < 10e6; i++)
+    {
+      ecto_push_ups::add2(std::rand(), std::rand());
+    }
+  }
+
+  std::cout << "big data (10e3 times)\n"
+      "size_t big_data();\n"
+      "{   std::vector<int> data;\n"
+      "    data.resize(10e4);\n"
+      "    for (size_t i = 0; i < 10e4; i++);\n"
+      "      data[i] = std::rand();\n"
+      "    return std::accumulate(data.begin(), data.end(), 0);\n"
+      "}" << std::endl;
+  std::cout << "ecto" << std::endl;
+  {
+    boost::progress_timer t; // start timing
+    for (size_t i = 0; i < 10e3; i++)
+    {
+      p.markDirty(b);
+      p.go(b);
+    }
+  }
+  std::cout << "raw" << std::endl;
+  {
+    boost::progress_timer t; // start timing
+    for (size_t i = 0; i < 10e3; i++)
+    {
+      ecto_push_ups::big_data();
+    }
+  }
+
+}
---
+++ test/benchmarks/push_ups.cpp
@@ -0,0 +1,63 @@
+#include <ecto/ecto.hpp>
+#include <iostream>
+#include <boost/format.hpp>
+#include <boost/foreach.hpp>
+
+#include <vector>
+#include <numeric>
+
+namespace ecto_push_ups
+{
+  size_t big_data()
+  {
+    std::vector<int> data;
+    data.resize(10e4);
+    for (size_t i = 0; i < 10e4; i++)
+      data[i] = std::rand();
+    return std::accumulate(data.begin(), data.end(), 0);
+  }
+  int add2(int x, int y)
+  {
+    return x + y;
+  }
+
+  struct Add2 : ecto::module
+  {
+    void Config()
+    {
+      SHOW();
+      setOut<int> ("out", "x+y");
+    }
+    void Process()
+    {
+      getOut<int> ("out") = add2(std::rand(),std::rand());
+    }
+    static void Params(tendrils_t& p)
+    {
+      SHOW();
+    }
+  };
+  struct BigData : ecto::module
+  {
+    void Config()
+    {
+      SHOW();
+      setOut<int> ("out", "sum of random array");
+    }
+    void Process()
+    {
+      getOut<int> ("out") = big_data();
+    }
+    static void Params(tendrils_t& p)
+    {
+      SHOW();
+    }
+  };
+
+}
+
+ECTO_MODULE(push_ups)
+{
+  using namespace ecto_push_ups;
+  ecto::wrap<Add2>("Add2", "An adder.");
+}
