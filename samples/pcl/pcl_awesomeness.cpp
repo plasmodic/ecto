5edcf1872298f989e8a0d43901280bdafe411150
Ethan Rublee <ethan.rublee@gmail.com>
2011-04-02 00:39:16 -0700

    adding voxel grid.

--- samples/pcl/pcl_awesomeness.cpp
+++ samples/pcl/pcl_awesomeness.cpp
@@ -7,10 +7,12 @@
 #include <pcl/io/kinect_grabber.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 #include <boost/shared_ptr.hpp>
 #include <pcl/visualization/cloud_viewer.h>
 #include <iostream>
 #include <boost/thread.hpp>
+
 
 typedef pcl::PointCloud<pcl::PointXYZRGB> cloud_t;
 class SimpleKinectGrabber
@@ -54,7 +56,9 @@ public:
   cloud_t::ConstPtr getLatestCloud()
   {
     boost::mutex::scoped_lock lock(mutex_);
-    return cloud_;
+    cloud_t::ConstPtr p = cloud_;
+    cloud_.reset();
+    return p;
   }
   void cloud_cb_(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
   {
@@ -81,11 +85,44 @@ struct KinectGrabber : ecto::module
 
   void Process()
   {
-    getOut<cloud_t::ConstPtr> ("out") = grabber_.getLatestCloud();
+    cloud_t::ConstPtr p;
+    while (!p)
+    {
+      p = grabber_.getLatestCloud();
+      boost::thread::yield();
+    }
+    getOut<cloud_t::ConstPtr> ("out") = p;
   }
   SimpleKinectGrabber grabber_;
 };
 
+struct VoxelGrid : ecto::module
+{
+
+  static void Params(tendrils_t& params)
+  {
+    params.set<float> ("leaf_size", "The size of the leaf(meters), smaller means more points...", 0.05);
+  }
+
+  void Config()
+  {
+    setIn<cloud_t::ConstPtr> ("in", "The cloud to filter");
+    setOut<cloud_t::ConstPtr> ("out", "Filtered cloud.");
+    float leaf_size = getParam<float>("leaf_size");
+    voxel_grid_.setLeafSize(leaf_size,leaf_size,leaf_size);
+    cloud_out_.reset(new cloud_t);
+  }
+
+  void Process()
+  {
+    cloud_t::ConstPtr cloud = getIn<cloud_t::ConstPtr> ("in");
+    voxel_grid_.setInputCloud(cloud);
+    voxel_grid_.filter(*cloud_out_);
+    getOut<cloud_t::ConstPtr>("out") = cloud_out_;
+  }
+  pcl::VoxelGrid<cloud_t::PointType> voxel_grid_;
+  cloud_t::Ptr cloud_out_;
+};
 struct CloudViewer : ecto::module
 {
   void Config()
@@ -115,7 +152,7 @@ struct CloudViewer : ecto::module
 
 ECTO_MODULE(pcl)
 {
-  // ecto::wrap<VoxelGrid>("VoxelGrid");
+  ecto::wrap<VoxelGrid>("VoxelGrid");
   ecto::wrap<KinectGrabber>("KinectGrabber", "This grabs frames from the kinect!!!");
   ecto::wrap<CloudViewer>("CloudViewer");
 }
