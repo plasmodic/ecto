0d311095720f4c5e2c3829a17ceadec43b61d8e4
Troy Straszheim <straszheim@willowgarage.com>
2011-04-13 09:46:57 -0700

    wrap tendrils separately, cleanup code layout a bit

--- src/pybindings/ecto.cpp
+++ src/pybindings/ecto.cpp
@@ -1,9 +1,15 @@
-#include "ecto_split.h"
-
 #include <boost/python.hpp>
 
-
 namespace bp = boost::python;
+
+namespace ecto {
+  namespace py {
+    void wrapTendrils();
+    void wrapConnection();
+    void wrapPlasm();
+    void wrapModule();
+  }
+}
 
 BOOST_PYTHON_MODULE(ecto)
 {
@@ -11,6 +17,7 @@ BOOST_PYTHON_MODULE(ecto)
   ecto::py::wrapConnection();
   ecto::py::wrapPlasm();
   ecto::py::wrapModule();
+  ecto::py::wrapTendrils();
 }
 
 
--- src/pybindings/module.cpp
+++ src/pybindings/module.cpp
@@ -55,50 +55,8 @@ namespace ecto
       }
     };
 
-    void setTendril(tendrils& t, const std::string& name, const std::string& doc, bp::object o)
-    {
-      t.declare<bp::object>(name, doc, o);
-    }
-
-    bp::object getTendril(tendrils& t, const std::string& name)
-    {
-      return t[name].extract();
-    }
-
-    std::string strTendril(const tendrils& t)
-    {
-      std::string s = "tendrils:\n";
-      for (tendrils::const_iterator iter = t.begin(), end = t.end();
-	   iter != end;
-	   ++iter)
-	{
-	  s += "    " + iter->first + " [" + iter->second.type_name() + "]\n";
-	}
-      return s;
-    }
-
-    bp::object tendril_get(const tendrils& ts, const std::string& name)
-    {
-      const tendril& t = ts.at(name);
-      return t.extract();
-    }
-
-    void tendril_set(tendrils& ts, const std::string& name, bp::object obj)
-    {
-      ts.at(name).set(obj);
-    }
-
     void wrapModule()
     {
-      bp::class_<tendrils, boost::shared_ptr<tendrils>, boost::noncopyable>("tendrils")
-	.def(bp::std_map_indexing_suite<tendrils, false>())
-	.def("set", setTendril)
-	.def("get", getTendril)
-	.def("__str__", strTendril)
-	.def("__getattr__", &tendril_get)
-	.def("__setattr__", &tendril_set)
-	;
-
       bp::class_<module, boost::shared_ptr<module>, boost::noncopyable>("module_cpp");
 
       bp::class_<modwrap, boost::shared_ptr<modwrap>, boost::noncopyable>("module_base"/*, bp::no_init*/)
