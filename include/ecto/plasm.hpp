67757ed07f037b07f194708402fde392de4aec1e
Ethan Rublee <ethan.rublee@gmail.com>
2011-04-20 02:52:48 -0700

    Update install.

--- include/ecto/plasm.hpp
+++ include/ecto/plasm.hpp
@@ -13,19 +13,30 @@ namespace ecto
   class module;
   typedef boost::shared_ptr<module> module_ptr;
 
+  /**
+   * \brief The plasm is the graph structure of ecto, responsible for keeping track
+   * of the connectivity between module and the execution of modules in the context of this
+   * graph.
+   *
+   * The plasm is mean to be tool interacted with from python, but may be useful
+   * from c++ in a dynamicly loaded environment.
+   */
   class plasm : boost::noncopyable
   {
   public:
+    /**
+     * vertex type enum
+     */
     enum vertex_t
     {
       root, input, output, param
     };
+
     typedef std::map<int, boost::tuple<module_ptr, vertex_t, std::string, tendril> > vertex_map_t;
     typedef std::list<boost::tuple<size_t, size_t> > edge_list_t;
 
     plasm();
-    void connect(boost::shared_ptr<module> from, const std::string& output, boost::shared_ptr<module> to,
-                 const std::string& input);
+    void connect(module_ptr from, const std::string& output, module_ptr to, const std::string& input);
     void mark_dirty(const boost::shared_ptr<module>& m);
     void go(const boost::shared_ptr<module>& m);
     void viz(std::ostream& out) const;
