2cd218448c64798e8cd8c854f4ee3ad1c0384dd0
Ethan Rublee <ethan.rublee@gmail.com>
2011-05-13 20:30:35 +0800

    working on cleanup.

--- include/ecto/tendrils.hpp
+++ include/ecto/tendrils.hpp
@@ -15,7 +15,29 @@ public:
   void declare(const std::string& name,
       const std::string& doc = "TODO: doc str me.", const T& default_val = T()) const
   {
-    const_cast<tendrils*> (this)->operator [](name) = tendril(default_val, doc);
+    map_t::const_iterator it = find(name);
+    //if there are no exiting tendrils by the given name,
+    //just add it.
+    if (it == end())
+    {
+      const_cast<tendrils*> (this)->insert(
+          std::make_pair(name, tendril(default_val, doc)));
+    }
+    else // we want to just return the existing tendril (so that modules preconnected don't get messed up)...
+    {
+      //there is already an existing tendril with the given name
+      //check if the types are the same
+      if (!it->second.is_type<T> ())
+      {
+        std::stringstream ss;
+        ss
+            << "Your types aren't the same, this could lead to very undefined behavior...";
+        ss << " old type = " << it->second.type_name() << " new type = "
+            << name_of<T> () << std::endl;
+        throw std::logic_error(ss.str());
+      }
+      const_cast<tendril&> (it->second).get<T> () = default_val;
+    }
   }
 
   template<typename T>
