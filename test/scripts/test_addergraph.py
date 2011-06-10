890580ca69471cc362b7313b33f6b9fac9dcf3dd
Troy D. Straszheim <straszheim@willowgarage.com>
2011-06-10 16:40:04 -0700

    WORD!

---
+++ test/scripts/test_addergraph.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import ecto
+import ecto_test
+import sys
+
+def build_addergraph(nlevels):
+    
+    plasm = ecto.Plasm()
+    
+    generators = [ecto_test.Generate("Generator 0_%u" % x, step=1.0, start=0.0)
+                  for x in range(2**nlevels)]
+    prevlevel = [ecto_test.Add("Adder 0_%u" % x) for x in range(2**(nlevels-1))]
+    for adder in prevlevel:
+        g = ecto_test.Generate("Generator", step=1.0, start=1.0)
+        plasm.connect(g, "out", adder, "left")
+        g = ecto_test.Generate("Generator", step=1.0, start=1.0)
+        plasm.connect(g, "out", adder, "right")
+
+    print "prev has", len(prevlevel)
+
+    for k in range(nlevels-2, -1, -1):
+        print "****** k=", k, " ***********"
+        thislevel = [ecto_test.Add("Adder %u_%u" % (k, x)) for x in range(2**k)]
+        print "prevlevel=", prevlevel
+        print "thislevel=", thislevel
+        index = 0
+        print "for...", range(2**k)
+        for r in range(2**k):
+            print "prev[%u] => cur[%u]" % (index, r)
+            plasm.connect(prevlevel[index], "out", thislevel[r], "left")
+            index += 1
+            print "prev[%u] => cur[%u]" % (index, r)
+            plasm.connect(prevlevel[index], "out", thislevel[r], "right")
+            index += 1
+        prevlevel = thislevel
+
+    assert len(prevlevel) == 1
+    final_adder = prevlevel[0]
+    printer = ecto_test.Printer("printy!")
+    plasm.connect(final_adder, "out", printer, "in")
+    return (plasm, final_adder)
+
+def test_plasm(nlevels, nthreads, niter):
+    (plasm, outnode) = build_addergraph(nlevels)
+
+    o = open('graph.dot', 'w')
+    print >>o, plasm.viz()
+    o.close()
+    print "\n", plasm.viz(), "\n"
+    sched = ecto.schedulers.Threadpool(plasm)
+    sched.execute(nthreads, niter)
+
+    print "RESULT:", outnode.outputs.out
+
+    shouldbe = float(2**nlevels * niter)
+    print "expected:", shouldbe
+    assert outnode.outputs.out == shouldbe
+
+    return
+    sched.execute(nthreads, niter)
+    result = outnode.outputs.out
+    print "RESULT:", result
+    
+    shouldbe = shouldbe + float(2**nlevels * niter)
+    assert result == shouldbe
+                     
+    return
+
+
+if __name__ == '__main__':
+    test_plasm(1, 1, 1)
+    test_plasm(1, 1, 2)
+    test_plasm(8, 1, 5)
+    test_plasm(9, 64, 100)
+    #test_plasm(1, 1, 1)
+    #test_plasm(1, 1, 4)
+
+
+
+
