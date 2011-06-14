a3b237c03e42c627823fd8723338e081f5d497df
Troy Straszheim <straszheim@willowgarage.com>
2011-06-14 07:54:53 -0700

    refactor into runandjoin, worker, propagator

--- src/asio_throwthread.cpp
+++ src/asio_throwthread.cpp
@@ -21,41 +21,76 @@
 
 using namespace boost;
 
-void worker() 
+struct thrower
 {
-  static unsigned offset = 0;
-  offset += 125;
-  asio::io_service serv;
-  asio::deadline_timer dt(serv);
-  for (unsigned j=0; j<4; ++j)
-    {
-      std::cout << this_thread::get_id() << " " << j << "\n";
-      dt.expires_from_now(posix_time::milliseconds(500 + offset));
-      dt.wait();
-    }
-  BOOST_THROW_EXCEPTION(std::runtime_error("OMGTHROW"));
-}
-
-struct thrower {
   exception_ptr eptr;
   thrower(exception_ptr eptr_) : eptr(eptr_) { }
 
-  void operator()() {
-    std::cout << "thrower is rethrowing\n";
+  void operator()() const
+  {
+    std::cout << "thrower is rethrowing" << std::endl;
     rethrow_exception(eptr);
   }
 };
 
+struct worker 
+{
+  asio::io_service& serv;
+  asio::io_service::work work;
+
+  unsigned ms;
+
+  worker(asio::io_service& serv_, unsigned ms_) 
+    : serv(serv_), work(serv), ms(ms_) 
+  { }
+
+  void operator()()
+  {
+    asio::io_service serv;
+    asio::deadline_timer dt(serv);
+    for (unsigned j=0; j<4; ++j)
+      {
+        std::cout << this_thread::get_id() << " " << j << "\n";
+        dt.expires_from_now(posix_time::milliseconds(ms));
+        dt.wait();
+      }
+    BOOST_THROW_EXCEPTION(std::runtime_error("OMGTHROW"));
+  }
+
+  template <typename Handler>
+  void post(Handler h)
+  {
+    serv.post(h);
+  }
+};
+
+struct propagator
+{
+  asio::io_service &from, &to;
+  asio::io_service::work work;
+
+  propagator(asio::io_service& from_, asio::io_service& to_) 
+  : from(from_), to(to_), work(to) { }
+
+  void operator()() {
+    from.run();
+  }
+
+  template <typename Handler>
+  void post(Handler h)
+  {
+    to.post(h);
+  }
+};
+
+
 struct runandjoin : enable_shared_from_this<runandjoin>
 {
   typedef shared_ptr<runandjoin> ptr;
+
   thread runner;
-  asio::io_service& serv;
-  asio::io_service::work work;
-  runandjoin(asio::io_service& serv_) : serv(serv_), work(serv)
-  { 
-    std::cout << this << " " << __PRETTY_FUNCTION__ << "\n";
-  }
+
+  runandjoin() { }
 
   ~runandjoin() {
     std::cout << this << " " << __PRETTY_FUNCTION__ << "\n";
@@ -75,11 +110,9 @@ struct runandjoin : enable_shared_from_this<runandjoin>
       w();
     } catch (const exception& e) {
       std::cout << "runandjoin caught:" << diagnostic_information(e) << std::endl;
-      thrower th(current_exception());
-      serv.post(th);
-      return;
+      w.post(thrower(current_exception()));
     }
-    serv.post(bind(&runandjoin::joinit, this, shared_from_this()));
+    w.post(bind(&runandjoin::joinit, this, shared_from_this()));
   }
 
   template <typename Work>
@@ -91,48 +124,6 @@ struct runandjoin : enable_shared_from_this<runandjoin>
   }
 };
 
-struct prop : enable_shared_from_this<prop> 
-{
-  typedef shared_ptr<prop> ptr;
-
-  asio::io_service &from, &to;
-  asio::io_service::work work;
-  thread runner;
-
-  prop(asio::io_service& from_, asio::io_service& to_) : from(from_), to(to_), work(to) { }
-  
-  void impl(ptr this_)
-  {
-    try {
-      std::cout << this << " running from\n";
-      from.run();
-      std::cout << this << " done running from\n";
-    } catch (const exception& e) {
-      std::cout << "POST RETHROWER\n";
-      to.post(thrower(current_exception()));
-      // post rethrower to 'to' serv
-    }
-    to.post(bind(&prop::joinit, this, shared_from_this()));
-
-  }
-
-  void joinit(ptr this_) 
-  {
-    std::cout << this << " " << __PRETTY_FUNCTION__ << "\n";
-    runner.join();
-  }
-
-  void run() 
-  {
-    thread* newthread = new thread(bind(&prop::impl, this, shared_from_this()));
-    newthread->swap(runner);
-  }
-
-  ~prop() {
-    std::cout << this << " " << __PRETTY_FUNCTION__ << "\n";
-  }
-};
-
 int main()
 {
   std::cout << "start..." << std::endl;
@@ -141,18 +132,20 @@ int main()
 
   for (unsigned j=0; j<5; ++j)
     {
-      runandjoin::ptr rj(new runandjoin(workserv));
-      rj->run(&worker);
+      runandjoin::ptr rj(new runandjoin);
+      rj->run(worker(workserv, (j+1) * 150 + (j*15)));
     }
 
   std::cout << "running props...\n";
   for (unsigned j=0; j<3; ++j)
     {
-      prop::ptr pp(new prop(workserv, topserv));
-      pp->run();
+      //      prop::ptr pp(new prop(workserv, topserv));
+      //      pp->run();
+      runandjoin::ptr rj(new runandjoin);
+      rj->run(propagator(workserv, topserv));
     }
-
   std::cout << "running tops...\n";
+
   try {
     topserv.run();
   } catch (const exception& e) {
