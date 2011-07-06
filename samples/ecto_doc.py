ecb2e246c18c124c2668eaca9d0956bc6a6a7ffd
Ethan Rublee <ethan.rublee@gmail.com>
2011-07-06 16:06:55 -0700

    Adding ecto_doc.py fix for ecto_opencv and recursive subdirectories.

--- samples/ecto_doc.py
+++ samples/ecto_doc.py
@@ -14,6 +14,17 @@ if __name__ == '__main__':
     if len(os.sys.argv) == 2:
         module = os.sys.argv[1]
         m = __import__(module)
-        ecto.list_ecto_module(m)
+        ms = [(module,m)]
+        for loader, module_name, is_pkg in  pkgutil.walk_packages(m.__path__):
+            #print loader,module_name,is_pkg
+            module = loader.find_module(module_name).load_module(module_name)
+            ms.append((module_name,module))
+        
+        ecto_cells = []
+        for module_name,x in ms:
+            print "..",module_name
+            ecto_cells += ecto.list_ecto_module(x)
+        #print ecto_cells
+        
     else :
         print usage
