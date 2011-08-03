426c8b6593b1b1e2d21f0872f6807f91364faae2
Troy Straszheim <straszheim@willowgarage.com>
2011-08-03 10:20:01 -0700

    make this guy a little less verbose

--- test/scripts/test_random.py
+++ test/scripts/test_random.py
@@ -13,7 +13,7 @@ def test_random():
     plasm.connect(uni, "out", printer, "in")
     
     sched = ecto.schedulers.Singlethreaded(plasm)
-    sched.execute(niter=100000)
+    sched.execute(niter=100)
     while sched.running():
         print "."
         time.sleep(0.1)
