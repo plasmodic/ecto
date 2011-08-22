48b90ddf44f4b648a47326b9d236f3bb84422291
Ethan Rublee <ethan.rublee@gmail.com>
2011-08-21 22:57:00 -0700

    Prefer the target properties over add_definitions, as that not all targets are forced to use the defitions.

--- kitchen/cmake/fastidious.cmake
+++ kitchen/cmake/fastidious.cmake
@@ -4,21 +4,27 @@ option(USE_FASTIDIOUS_FLAGS
 
 if(USE_FASTIDIOUS_FLAGS)
   if(UNIX)
-    set(FASTIDIOUS_FLAGS -Werror -Wall -Wno-non-virtual-dtor)
-
+    #be sure to keep the flags as a string and not as cmake list.
+    #These are used in the ectomodule macro like, so that not all targets are
+    #forced to use them.
+    #
+    #set_target_properties(mytarget
+    #  PROPERTIES
+    #  COMPILE_FLAGS "${FASTIDIOUS_FLAGS}"
+    #  )
+    set(FASTIDIOUS_FLAGS "-Werror -Wall")
     if(GCC_VERSION VERSION_GREATER 4.4 OR GCC_VERSION VERSION_EQUAL 4.4)
-      list(APPEND FASTIDIOUS_FLAGS -Wl,--Wno-undefined)
+      set(FASTIDIOUS_FLAGS "${FASTIDIOUS_FLAGS} -Wl,--Wno-undefined")
+    else()
+      set(FASTIDIOUS_FLAGS "${FASTIDIOUS_FLAGS} -Wno-non-virtual-dtor")
     endif()
-
+    
+    #apple is unix
+    if(APPLE)
+        #set(FASTIDIOUS_FLAGS "")
+    endif()
+    
   elseif(WIN32)
-
     set(FASTIDIOUS_FLAGS "/D _CRT_SECURE_NO_WARNINGS /D BOOST_HAS_STDINT_H /wd4099 /wd4101 /wd4996 /wd4251 /wd4305 /D _WIN32_WINNT=0x0501")
-
-  elseif(APPLE)
-
-    set(FASTIDIOUS_FLAGS "")
-
   endif()
 endif()
-
-add_definitions (${FASTIDIOUS_FLAGS})
