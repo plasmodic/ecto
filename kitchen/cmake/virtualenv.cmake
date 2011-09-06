773cad9df2e1a4bb4c308497128f313304610eb3
Ethan Rublee <ethan.rublee@gmail.com>
2011-09-05 23:54:09 -0700

    Disable doc install.

--- kitchen/cmake/virtualenv.cmake
+++ kitchen/cmake/virtualenv.cmake
@@ -1,6 +1,17 @@
 macro(setup_virtualenv PROJECT)
   option(INSTALL_TO_VIRTUALENV "Install by default to a virtual environment" ON)
-
+  option(WORK_IN_VIRTUALENV "Install by default to a virtual environment" OFF)
+  if(WORK_IN_VIRTUALENV)
+    if("$ENV{VIRTUAL_ENV}" STREQUAL "")
+      message(WARNING "No active virtualenv!")
+    else()
+      set(VIRTUALENV_DIR $ENV{VIRTUAL_ENV} CACHE PATH "VIRTUALENV path" FORCE)
+      set(INSTALL_TO_VIRTUALENV OFF CACHE BOOL "" FORCE) #MUTUALLY EXCLUSIVE
+      set(CMAKE_INSTALL_PREFIX ${VIRTUALENV_DIR} CACHE PATH "" FORCE)
+      set(CMAKE_INSTALL_RPATH  ${VIRTUALENV_DIR}/lib CACHE PATH "" FORCE)
+      message(STATUS "virtualenv path:       ${VIRTUALENV_DIR}")
+    endif()
+  endif()
   if(INSTALL_TO_VIRTUALENV)
     find_program(VIRTUALENV_EXECUTABLE
       NAMES virtualenv
@@ -9,8 +20,6 @@ macro(setup_virtualenv PROJECT)
     mark_as_advanced(VIRTUALENV_EXECUTABLE)
 
     set(VIRTUALENV_DIR  "/opt/ecto/${PROJECT}/${${PROJECT}_GITTAG_SHORT}" CACHE PATH "Virtual environment")
-
-
 
     option(VIRTUALENV_ALL "Add virtualenv to all" off)
     mark_as_advanced(VIRTUALENV_ALL)
@@ -28,6 +37,7 @@ macro(setup_virtualenv PROJECT)
     add_custom_command(TARGET virtualenv
       COMMAND ${PYTHON_EXECUTABLE} bootstrap.py ${VIRTUALENV_DIR}
     )
+    
     add_dependencies(virtualenv bootstrap)
     set(CMAKE_INSTALL_PREFIX ${VIRTUALENV_DIR} CACHE PATH "" FORCE)
     set(CMAKE_INSTALL_RPATH  ${VIRTUALENV_DIR}/lib CACHE PATH "" FORCE)
