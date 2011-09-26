3a4ece7bdb6812300cc2caf887dbab6909e1a172
Troy Straszheim <straszheim@willowgarage.com>
2011-09-26 08:23:46 -0700

    Attempt to diagnose strange problems on lucid.  No idea if this will work.

--- cmake/ectoFindDeps.cmake
+++ cmake/ectoFindDeps.cmake
@@ -1,18 +1,5 @@
 find_package(Threads)
 
-find_package(Boost COMPONENTS
-  python
-  thread
-  regex
-  system
-  REQUIRED
-  )
-
-#if (Boost_FOUND)
-#  message(STATUS "Boost headers at ${Boost_INCLUDE_DIRS}")
-#  message(STATUS "Boost libraries at ${Boost_LIBRARIES}")
-#endif()
-
 find_package(PythonLibs REQUIRED)
 
 include_directories(
@@ -22,14 +9,14 @@ include_directories(
 
 if(WIN32)
   link_directories(${Boost_LIBRARY_DIRS})
-  set(ECTO_DEP_LIBS 
+  set(ECTO_DEP_LIBS
     ${PYTHON_LIBRARIES}
     ${CMAKE_THREAD_LIBS_INIT}
     ${Boost_PYTHON_LIBRARY}
     CACHE INTERNAL "Libraries dependencies" FORCE
   )
 else()
-  set(ECTO_DEP_LIBS 
+  set(ECTO_DEP_LIBS
     ${Boost_LIBRARIES}
     ${PYTHON_LIBRARIES}
     ${CMAKE_THREAD_LIBS_INIT}
@@ -43,7 +30,7 @@ find_package(PythonInterp REQUIRED)
 execute_process(COMMAND ${PYTHON_EXECUTABLE} --version
           ERROR_VARIABLE PYTHON_VERSION_FULL
           OUTPUT_STRIP_TRAILING_WHITESPACE)
-          
+
 string(REGEX MATCH "[0-9].[0-9]" PYTHON_VERSION_MAJOR_MINOR "${PYTHON_VERSION_FULL}")
 mark_as_advanced(PYVERSIONS_EXE)
 if(UNIX)
