7771c6be79532359da0ec495aacb7a3902f14ca1
Ethan Rublee <ethan.rublee@gmail.com>
2011-10-01 18:43:40 -0700

    Update changelog generation file.

--- doc/source/changelog_gen.py
+++ doc/source/changelog_gen.py
@@ -11,11 +11,11 @@ tags = ('amoeba-beta2',
 pairs = []
 for i in range(0,len(tags)-1):
     pairs += [(tags[i],tags[i+1])]
-print pairs
 pairs.reverse()
 print '''
 Changelog
 =========
+This is a generated changelog file.
 '''
 for tag1,tag2 in pairs:
     header = '%s..%s'%(tag1,tag2)
@@ -26,4 +26,8 @@ for tag1,tag2 in pairs:
                        '--oneline',
                        '--no-color',
                        '%s..%s'%(tag1,tag2)], stdout=subprocess.PIPE)
-    print p.communicate()[0]
+    lines = [x.split(' ',1) for x in p.communicate()[0].strip().split('\n') ]
+    # '* `%(ref)s <https://github.com/plasmodic/ecto/commit/%(ref)s>`_ %(comment)s` %s'%
+    lines = ['* `%(ref)s <https://github.com/plasmodic/ecto/commit/%(ref)s>`_ %(comment)s'%dict(ref=x[0],comment=x[1]) for x in lines]
+    print '\n'.join(lines)
+    print '\n'
