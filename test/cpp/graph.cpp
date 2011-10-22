3cb4a66cbc6a65452d25f5d243f4bccb1bd52b11
Troy Straszheim <straszheim@willowgarage.com>
2011-10-21 21:05:09 -0700

    switch to enable compile tests

--- test/cpp/graph.cpp
+++ test/cpp/graph.cpp
@@ -72,7 +72,7 @@ TEST(Plasm, Passthrough)
   EXPECT_TRUE(out == 5.0);
 }
 
-TEST(Plasm,Registry)
+TEST(Plasm, Registry)
 {
   ecto::cell::ptr add = ecto::registry::create("ecto_test::Add");
   EXPECT_TRUE(add);
