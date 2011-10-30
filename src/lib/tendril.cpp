ff2ca91b630e97b8b71e15789540351ea38ad804
Ethan Rublee <ethan.rublee@gmail.com>
2011-10-30 12:49:29 -0700

    Removing special case for opencv types.

--- src/lib/tendril.cpp
+++ src/lib/tendril.cpp
@@ -190,23 +190,7 @@ namespace ecto
 
   void tendril::copy_holder(const tendril& rhs)
   {
-#if 1
-    if(!is_type<none>() && is_type<cv::Mat>())
-    {
-      rhs.get<cv::Mat>().copyTo(get<cv::Mat>());
-    }else if(!is_type<none>() && is_type<std::vector<cv::Mat> >())
-    {
-      std::cout << "vecs of mats" << std::endl;
-      const std::vector<cv::Mat>& ms = rhs.get<std::vector<cv::Mat> >();
-      std::vector<cv::Mat>& toms = get<std::vector<cv::Mat> >();
-      toms.resize(ms.size());
-      for(size_t i = 0,end =ms.size(); i < end; i++)
-      {
-        ms[i].copyTo(toms[i]);
-      }
-    }else
-#endif
-      holder_ = rhs.holder_;
+    holder_ = rhs.holder_;
     type_ID_ = rhs.type_ID_;
     converter = rhs.converter;
   }
