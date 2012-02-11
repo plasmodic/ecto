37aec33fea92e6498a6f6c942015cccef855ad08
Ethan Rublee <ethan.rublee@gmail.com>
2012-02-10 20:06:25 -0800

    gui playing

---
+++ python/ecto/gui.py
@@ -0,0 +1,101 @@
+import ecto
+import sys
+
+HAS_GUI=False
+
+try:
+    from PySide.QtCore import QTimer
+    from PySide.QtGui import QWidget, QDialog, QPushButton, QApplication, \
+     QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QScrollArea, QMainWindow, QSizePolicy
+    HAS_GUI=True
+    class TendrilWidget(QWidget):
+        def __init__(self, name, tendril, parent=None):
+            super(TendrilWidget,self).__init__(parent)
+            hlayout = QHBoxLayout()
+            hlayout.addWidget(QLabel(name))
+            edit = QLineEdit(str(tendril.val))
+            hlayout.addWidget(edit)
+
+            self.thunker = TendrilThunker(tendril)
+            edit.textChanged.connect(self.thunker.update)
+
+            self.setLayout(hlayout)
+            self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+    class DynamicReconfigureForm(QWidget):
+        def __init__(self, plasm, parent=None):
+            super(DynamicReconfigureForm, self).__init__(parent)
+            self.plasm = plasm
+            self.setWindowTitle("Dynamic Reconfigure")
+            plasm.configure_all()
+            self.generate_dialogs()
+            self.sched = ecto.schedulers.Singlethreaded(plasm)
+
+        def exec_one(self):
+            #hack to get around GIL issues.
+            #TODO remove this
+            rval = self.sched.execute(niter=1)
+            if rval:
+                sys.exit(rval)
+
+        def generate_dialogs(self):
+            vlayout = QVBoxLayout()
+            commit_button = QPushButton("&Commit")
+            w_all = QWidget()
+            v = QVBoxLayout()
+            for cell in self.plasm.cells():
+                w = QWidget()
+                cvlayout = QVBoxLayout()
+                cvlayout.addWidget(QLabel(cell.name()))
+                for name, tendril in cell.params:
+                    tw = TendrilWidget(name, tendril)
+                    cvlayout.addWidget(tw)
+                    commit_button.clicked.connect(tw.thunker.commit)
+                w.setLayout(cvlayout)
+#                w.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+                v.addWidget(w)
+            w_all.setLayout(v)
+            s = QScrollArea()
+            s.setWidget(w_all)
+            vlayout.addWidget(s)
+            vlayout.addWidget(commit_button)
+            self.setLayout(vlayout)
+
+
+except ImportError as e:
+    print e
+    pass
+
+
+class TendrilThunker(object):
+
+    def __init__(self, tendril):
+        self.tendril = tendril
+        self.val = None
+
+    def update(self, val):
+        self.val = val
+
+    def commit(self):
+        if self.val:
+            x = type(self.tendril.val)(self.val)
+            self.tendril.set(x)
+            self.val = None
+
+
+def gui_execute(plasm):
+    if HAS_GUI:
+        # Create a Qt application
+        app = QApplication(sys.argv)
+        # Create and show the form
+        form = DynamicReconfigureForm(plasm)
+        timer = QTimer(form)
+        timer.timeout.connect(form.exec_one)
+        timer.start()
+        form.show()
+        # Run the main Qt loop
+        return app.exec_()
+
+    else:
+        print >>sys.stderr, "Could not import PySide. Please install python-pyside."
+        sys.exit(1)
