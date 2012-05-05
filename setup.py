c0fcca8b3616f9e7a6699af4836b4f5a0a6ae8b8
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-05 08:44:05 -0700

    add the sphinx folders to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from distutils.core import setup
 setup(name='ecto',
       version='1.0.0',
       description='Ecto is a hybrid C++/Python development framework for constructing and maintaining pipelines.',
-      packages=['ecto'],
+      packages=['ecto', 'ecto.sphinx', 'ecto.sphinx.breathe',
+                'ecto.sphinx.breathe.finder', 'ecto.sphinx.breathe.finder.doxygen',
+                'ecto.sphinx.breathe.parser', 'ecto.sphinx.breathe.parser.doxygen',
+                'ecto.sphinx.breathe.renderer', 'ecto.sphinx.breathe.renderer.rst', 'ecto.sphinx.breathe.renderer.rst.doxygen',
+               ],
       package_dir={'':'python'}
 )
