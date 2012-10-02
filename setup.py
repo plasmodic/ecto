823354a61ccc044b2933abe7b7170303e31b7712
Vincent Rabaud <vrabaud@willowgarage.com>
2012-10-02 16:33:24 +0200

    simpler setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from catkin_pkg.package import parse_package_for_distutils
 
-setup(name='ecto',
-      version='1.0.0',
-      description='Ecto is a hybrid C++/Python development framework for constructing and maintaining pipelines.',
-      packages=['ecto', 'ecto.sphinx', 'ecto.sphinx.breathe',
+d = parse_package_for_distutils()
+d['packages'] = ['ecto', 'ecto.sphinx', 'ecto.sphinx.breathe',
                 'ecto.sphinx.breathe.finder', 'ecto.sphinx.breathe.finder.doxygen',
                 'ecto.sphinx.breathe.parser', 'ecto.sphinx.breathe.parser.doxygen',
                 'ecto.sphinx.breathe.renderer', 'ecto.sphinx.breathe.renderer.rst', 'ecto.sphinx.breathe.renderer.rst.doxygen',
-               ],
-      package_dir={'':'python'}
-)
+               ]
+d['package_dir'] = {'': 'python'}
+d['install_requires'] = []
+
+setup(**d)
