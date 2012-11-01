59fa00167742945bfb633c5b206f371db1a7d681
Vincent Rabaud <vrabaud@willowgarage.com>
2012-11-01 07:03:40 -0700

    make sure the information is parsed from the package.xml

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-from distutils.core import setup
 
-setup(name='ecto',
-      version='1.0.0',
-      description='Ecto is a hybrid C++/Python development framework for constructing and maintaining pipelines.',
-      packages=['ecto', 'ecto.sphinx', 'ecto.sphinx.breathe',
+from distutils.core import setup
+from ecto.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['ecto', 'ecto.sphinx', 'ecto.sphinx.breathe',
                 'ecto.sphinx.breathe.finder', 'ecto.sphinx.breathe.finder.doxygen',
                 'ecto.sphinx.breathe.parser', 'ecto.sphinx.breathe.parser.doxygen',
                 'ecto.sphinx.breathe.renderer', 'ecto.sphinx.breathe.renderer.rst', 'ecto.sphinx.breathe.renderer.rst.doxygen',
-               ],
-      package_dir={'':'python'}
-)
+               ]
+d['package_dir'] = {'': 'python'}
+d['install_requires'] = []
+
+setup(**d)
