2a01641a8bf0cf853bfe0ffa0c7090721845ac36
Vincent Rabaud <vincent.rabaud@gmail.com>
2014-02-16 15:50:55 +0100

    fix compilation on Saucy

--- src/lib/except.cpp
+++ src/lib/except.cpp
@@ -172,7 +172,7 @@ namespace ecto
           for( error_info_map::const_iterator i=info_.begin(),end=info_.end(); i!=end; ++i )
             {
               boost::shared_ptr<error_info_base const> const & x = i->second;
-#if ((BOOST_VERSION / 100) % 1000) <= 50
+#if ((BOOST_VERSION / 100) % 1000) <= 53
               tmp << str(fmt % /*name_of(*/x->tag_typeid_name()/*) */
                          % x->value_as_string());
 #else
